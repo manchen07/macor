83f27458f0cbbd1868d65552a79b82107e4b366d
jihyunl <63086933+jihyunl@users.noreply.github.com>
2020-04-18 15:52:41 -0500

    Change argument name to be consistent,
    Add tetrachoric cor

--- R/functions.R
+++ R/functions.R
@@ -1,38 +1,43 @@
-cal_d <- function(trt_m, trt_sd, trt_n, ctl_m, ctl_sd, ctl_n){
-  d <- (trt_m - ctl_m)/sqrt(((trt_n-1)*trt_sd^2 + (ctl_n-1)*ctl_sd^2)/(trt_n + ctl_n -2))
-  v_d <- (trt_n + ctl_n)/(trt_n*ctl_n) + d^2 /(2*(trt_n + ctl_n))
+# Calculate d
+cal_d <- function(m_trt, sd_trt, n_trt, m_ctl, sd_ctl, n_ctl){
+  d <- (m_trt - m_ctl)/sqrt(((n_trt-1)*sd_trt^2 + (n_ctl-1)*sd_ctl^2)/(n_trt + n_ctl -2))
+  v_d <- (n_trt + n_ctl)/(n_trt*n_ctl) + d^2 /(2*(n_trt + n_ctl))
   cbind(d, v_d)
 }
 
-cal_t <- function(n_trt, m_trt, sd_trt, m_ctl, sd_ctl, n_ctl){
+# Calculate t
+cal_t <- function(m_trt, sd_trt, n_trt, m_ctl, sd_ctl, n_ctl){
   sp <- sqrt(((n_trt-1)*sd_trt^2 + (n_ctl-1)*sd_ctl^2)/(n_trt + n_ctl -2))
   t <- (m_trt - m_ctl)/(sp*sqrt((1/n_ctl)+ (1/n_ctl)))
   t
 }
 
+## Calculate t to d - we may not need this. 
+cal_t_d <- function(t, n_trt, n_ctl){
+  d <- t*sqrt(1/n_trt + 1/n_ctl) 
+  d
+}
+
+
+# Controlled experiment 
 cal_rce <- function(d, n1, n2) {
   a <- (n1+n2)^2/(n1*n2)
   r_ce <- d/sqrt(d^2 + a^2)
   r_ce
 }
 
-# large sample variance of r_ce
+## large sample variance of r_ce
 cal_v_rce <- function(d, n1, n2){
   a <- (n1+n2)^2/(n1*n2)
   v_rce <- (a^4/(d^2 + a^2)^3)*((n1+n2)/(n1*n2) + (d^2)/(2*(n1+n2)))
   v_rce
 }
 
-
-## Calculate t to d - we may not need this. 
-cal_t_d <- function(t, trt_n, ctl_n){
-  d <- t*sqrt(1/trt_n + 1/ctl_n) 
-  d
-}
-
-# d to r_b
+# Biserial correlation
+## d to r_b
 dtob <- function(d, n1, n2){
   h <- (n1 + n2 -2)/n1 + (n1+n2-2)/n2
+  # point-biserial
   r_pb <- d/sqrt(d^2 + h)
   p <- n1/(n1+n2)
   c <- qnorm(p)
@@ -40,9 +45,10 @@ dtob <- function(d, n1, n2){
   return(r_b)
 }
 
-# t to r_b 
+## t to r_b 
 ttob <- function(t, n1, n2){
   m <- n1+n2-2
+  # point-biserial
   r_pb <- t/sqrt(t^2 + m)
   p <- n1/(n1+n2)
   c <- qnorm(p)
@@ -51,7 +57,7 @@ ttob <- function(t, n1, n2){
 }
 
 ### Sampling variance of r_b - Soper's approxiamte method.
-
+# r_b: biserial correlation
 cal_v_rb <- function(r_b, n1, n2) {
   p <- n1/(n1+n2)
   c <- qnorm(p)
@@ -59,9 +65,9 @@ cal_v_rb <- function(r_b, n1, n2) {
   return(v_rb)
 }
 
+
 # function from John Fox's polychor function
 # This uses ML estimator. 
-
 pcor <- function(x) {
   require(polycor)
   mat <- matrix(x, 2, 2)
@@ -69,7 +75,8 @@ pcor <- function(x) {
   c(pcor = pc$rho, v_pcor = pc$var[1,1])
 }
 
-# function from ?
+### Tetrachoric correlation - contingency table
+## (1) function from 
 calc_tet <- function(a00, a11, a10, a01) {
   or <- a00 * a11 / (a01 * a10)
   v_or <- 1/a00 + 1/a01 + 1/a10 + 1/a11
@@ -84,4 +91,20 @@ calc_tet <- function(a00, a11, a10, a01) {
   h1 <- dnorm(z1)
   v_tet <- (p0 * (1 - p0) * p1 * (1 - p1)) / (n * (h0 * h1)^2)
   return(cbind(or, v_or, r_tet, v_tet))
+}
+
+## (2) Odds ratio and martinal N
+# If we have OR and marginal Ns for y2 (n0, n1) and marginals for y1 (n_ctl, n_trt) and Total N
+calc_tet2 <- function(OR, n0, n_ctl, N){
+  delta <- 1 + sqrt(OR)
+  r_tet <- cos(pi / delta)
+  
+  p0 <- n0/N
+  p1 <- n_ctl/N
+  z0 <- qnorm(p0)
+  z1 <- qnorm(p1)
+  h0 <- dnorm(z0)
+  h1 <- dnorm(z1)
+  v_tet <- (p0 * (1 - p0) * p1 * (1 - p1)) / (N * (h0 * h1)^2)
+  return(cbind(r_tet, v_tet))
 }
